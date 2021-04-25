532c365002ec553f22d0851a978b9d6c32cfc4bf
Felipe Domingues <72865567+NucleoEF-1ano@users.noreply.github.com>
2021-04-25 20:21:09 -0300

    insert routes

--- src/App.js
+++ src/App.js
@@ -1,53 +1,67 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
-import Home from './Home';
-import { getUserData, getUserRepos } from './api';
-
-const mockData = {
-	login: 'felipeeu',
-	id: 22958295,
-	node_id: 'MDQ6VXNlcjIyOTU4Mjk1',
-	avatar_url: 'https://avatars.githubusercontent.com/u/22958295?v=4',
-	gravatar_id: '',
-	url: 'https://api.github.com/users/felipeeu',
-	html_url: 'https://github.com/felipeeu',
-	followers_url: 'https://api.github.com/users/felipeeu/followers',
-	following_url: 'https://api.github.com/users/felipeeu/following{/other_user}',
-	gists_url: 'https://api.github.com/users/felipeeu/gists{/gist_id}',
-	starred_url: 'https://api.github.com/users/felipeeu/starred{/owner}{/repo}',
-	subscriptions_url: 'https://api.github.com/users/felipeeu/subscriptions',
-	organizations_url: 'https://api.github.com/users/felipeeu/orgs',
-	repos_url: 'https://api.github.com/users/felipeeu/repos',
-	events_url: 'https://api.github.com/users/felipeeu/events{/privacy}',
-	received_events_url: 'https://api.github.com/users/felipeeu/received_events',
-	type: 'User',
-	site_admin: false,
-	name: 'Felipe Domingues ',
-	company: null,
-	blog: '',
-	location: null,
-	email: 'felipeeu@yahoo.com.br',
-	hireable: null,
-	bio: 'React JS freelance developer , chemistry teacher and science/technology enthusiast',
-	twitter_username: null,
-	public_repos: 30,
-	public_gists: 0,
-	followers: 10,
-	following: 4,
-	created_at: '2016-10-20T11:59:38Z',
-	updated_at: '2021-04-21T08:53:53Z'
-};
+import { getUserData, getUserRepos, getStarredRepos } from './api';
+import { Route, Switch, useHistory, useLocation } from 'react-router-dom';
+import Search from './components/Search';
+import Button from './components/Button';
+import User from './User';
+import Repos from './Repos';
+import Starred from './Starred';
 
 function App() {
 	const [ value, setValue ] = useState('');
+	const [ userData, setUserData ] = useState([]);
+	const [ repoData, setRepoData ] = useState([]);
+	const [ starredData, setStarredData ] = useState([]);
 
-	useEffect(() => {
-		getUserRepos('felipeeu');
-	}, []);
+	const location = useLocation();
+
+	const history = useHistory();
+
+	const fetchUser = (name) => {
+		if (name) {
+			getUserData(name)
+				.then((response) => {
+					if (Object.keys(response).length > 0) setUserData(response.data);
+				})
+				.catch((err) => err);
+			history.push(`/${name}`);
+		}
+	};
+	const fetchRepos = (name) => {
+		getUserRepos(name)
+			.then((response) => {
+				if (Object.keys(response).length > 0) setRepoData(response.data);
+			})
+			.catch((err) => err);
+		history.push(`/${name}/repos`);
+	};
+
+	const fetchStarred = (name) => {
+		getStarredRepos(name)
+			.then((response) => {
+				if (Object.keys(response).length > 0) setStarredData(response.data);
+			})
+			.catch((err) => err);
+		history.push(`/${name}/starred`);
+	};
 
 	return (
 		<div className="container-sm p-3">
-			<Home value={value} setValue={setValue} data={mockData} />
+			<Search value={value} setValue={setValue} onClick={() => fetchUser(value)} />
+			<Switch>
+				<Route path="/:user/starred">
+					<Button label={'Back'} onClick={() => history.push(`/${value}`)} />
+					<Starred />
+				</Route>
+				<Route path="/:user/repos">
+					<Button label={'Back'} onClick={() => history.push(`/${value}`)} />
+					<Repos data={repoData} />
+				</Route>
+				<Route path="/:user">
+					<User data={userData} fetchRepos={fetchRepos} fetchStarred={fetchStarred} />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
