2aa22ef2c10be6d5c3843250569a1f5b812a4dee
Felipe Domingues <72865567+NucleoEF-1ano@users.noreply.github.com>
2021-04-26 08:56:16 -0300

    insert no user screen

---
+++ src/NoUser.js
@@ -0,0 +1,11 @@
+const NoUser = ({ data, fetchRepos, fetchStarred }) => {
+	return (
+		<div className="card mb-3 mt-2 ">
+			<div className="card-body">
+				<h2 className="card-title">Any user was found</h2>
+			</div>
+		</div>
+	);
+};
+
+export default NoUser;
